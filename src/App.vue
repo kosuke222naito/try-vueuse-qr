c475b8d75508256077111de3c52183797e806351
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-08-01 15:25:02 +0900

    demo

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+import { useMouse, usePreferredDark } from "@vueuse/core";
+import { useQRCode } from '@vueuse/integrations/useQRCode';
+
+const { x, y } = useMouse();
+const isDark = usePreferredDark();
+
+const text = ref("text-to-encode");
+const qrcode = useQRCode(text)
+</script>
+
+<template>
+  {{ x }}, {{ y }}
+  {{ isDark }}
+
+  <div>
+    <input v-model="text" type="text" />
+    <img :src="qrcode" alt="QR Code" />
+  </div>
+
+</template>
+
+<style scoped></style>
